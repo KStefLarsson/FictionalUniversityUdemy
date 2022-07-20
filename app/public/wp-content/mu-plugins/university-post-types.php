70e9872de2d748247651ae10f1c84905ad3609ab
KStefLarsson <mille83.sl@gmail.com>
2022-07-20 12:24:52 +0200

    created a new mapp in wp-content namned mu-plugins and a new file inside of it namned university-post-type.php

---
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,19 @@
+<?php 
+    // En funktion som skapar en ny post_type (custom_post_type) som syns i wordpress dashboard.
+    function university_post_types() {
+        register_post_type('event', array(
+            'public' => true,       // Detta gör den synlig.
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'Events',  // Byter namn från Posts som är default namnet till Events.
+                'add_new_item' => 'Add New Event',  // Ändrar rubriken i wordpress dashboard från Add New Post till Add New Event.
+                'edit_item' => 'Edit Event',  // Ändrar rubriken i wordpress dashboard från Edit Post till Edit Event.
+                'all_items' => 'All Events',  // Ändrar rubriken i wordpress dashboard menyn från Events till All Events.
+                'singular_name' => 'Event'
+            ),
+            'menu_icon' => 'dashicons-calendar'  // Ändrar ikonen till en egen unik som jag själv väljer genom att googla på wordpress dashicons.
+        ));
+    }
+
+    add_action('init', 'University_post_types');
+?>
