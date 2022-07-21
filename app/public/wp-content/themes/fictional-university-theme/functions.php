09e6c695cc6e09630fde33c903d55c6525996603
KStefLarsson <mille83.sl@gmail.com>
2022-07-22 00:55:23 +0200

    Created a function in functions.php to handle the banner, images and titles

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,37 @@
 <?php
 
+    function pageBanner ($args = NULL) {
+        // php logic will live here
+        if (!$args['title']) {
+            $args['title'] = get_the_title();
+        }
+
+        if (!$args['subtitle']) {
+            $args['subtitle'] = get_field('page_banner_subtitle');
+        }
+
+        if (!$args['photo']) {
+            if (get_field('page_banner_background_image')) {
+                $args['photo'] = get_field('page_banner_background_image') ['sizes'] ['pageBanner'];
+            }
+            else {
+                $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+            }
+        }
+
+        ?>
+        <!-- denna div sätter bakgrundsbild till headern samt namn och subtittel på varje professor. -->
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+                <div class="page-banner__content container container--narrow">
+                    <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+                    <div class="page-banner__intro">
+                    <p><?php echo $args['subtitle']; ?></p>
+                    </div>
+            </div>
+        </div>
+    <?php }
+
     // En funktion för att inkludera css och javascript filer 
     function university_files() {
         wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true); //Laddar in javascript fil
@@ -22,6 +54,7 @@
         // Bestäm själv storlek på bilder
         add_image_size('professorLandscape', '400', '260', true);
         add_image_size('professorPortrait', '480', '650', true);
+        add_image_size('pageBanner', '1500', '350', true);
     }
 
     add_action('after_setup_theme', 'university_features');  // Anropar funktionen university_features
--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -2,17 +2,10 @@
     get_header();
 
     while (have_posts()) {
-        the_post(); ?>
-
-        <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-                <div class="page-banner__content container container--narrow">
-                    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-                    <div class="page-banner__intro">
-                    <p>Dont forget to replace me later.</p>
-                    </div>
-            </div>
-        </div>
+        the_post(); 
+        // denna div sätter bakgrundsbild till headern samt namn och subtittel på varje professor.
+        pageBanner();
+        ?>
 
         <!-- en box på en event sida med knapp tillbaka till alla events och en ruta med namnet på eventet -->
         <div class="container container--narrow page-section">
@@ -21,7 +14,7 @@
             <div class="generic-content">
                 <div class="row group">
                     <div class="one-third">
-                        <?php the_post_thumbnail(); ?>
+                        <?php the_post_thumbnail('professorPortrait'); ?>
                     </div>
                     <div class="two-thirds">
                     <?php the_content(); ?>
