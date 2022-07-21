6a76e225a5beb7c89d8f15f3c10d8922bec979ea
KStefLarsson <mille83.sl@gmail.com>
2022-07-21 21:26:47 +0200

    created logic in mu-plugins for professor

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -36,13 +36,34 @@
             'public' => true,       // Detta gör den synlig.
             'show_in_rest' => true,
             'labels' => array(
-                'name' => 'programs',  // Byter namn från Posts som är default namnet till Events.
+                'name' => 'Programs',  // Byter namn från Posts som är default namnet till Programs.
                 'add_new_item' => 'Add New Program',  // Ändrar rubriken i wordpress dashboard från Add New Post till Add New Event.
-                'edit_item' => 'Edit Program',  // Ändrar rubriken i wordpress dashboard från Edit Post till Edit Event.
-                'all_items' => 'All Programs',  // Ändrar rubriken i wordpress dashboard menyn från Events till All Events.
+                'edit_item' => 'Edit Program',  // Ändrar rubriken i wordpress dashboard från Edit Post till Edit Program.
+                'all_items' => 'All Programs',  // Ändrar rubriken i wordpress dashboard menyn från Program till All Program.
                 'singular_name' => 'Program'
             ),
             'menu_icon' => 'dashicons-awards'  // Ändrar ikonen till en egen unik som jag själv väljer genom att googla på wordpress dashicons.
+        ));
+
+        
+        // Professor Post Type
+
+        register_post_type('professor', array(
+            'supports' => array(
+                'title',
+                'editor',
+                'thumbnail' // Detta enablar featured image flik på respektive professor i wordpress dashboard.
+            ),
+            'public' => true,       // Detta gör den synlig.
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'Professor',  // Byter namn från Posts som är default namnet till Professor.
+                'add_new_item' => 'Add New professor',  // Ändrar rubriken i wordpress dashboard från Add New Post till Add New Professor.
+                'edit_item' => 'Edit professor',  // Ändrar rubriken i wordpress dashboard från Edit Post till Edit Professor.
+                'all_items' => 'All Professor',  // Ändrar rubriken i wordpress dashboard menyn från Professors till All Professors.
+                'singular_name' => 'professor'
+            ),
+            'menu_icon' => 'dashicons-welcome-learn-more'  // Ändrar ikonen till en egen unik som jag själv väljer genom att googla på wordpress dashicons.
         ));
     }
 
