464d7f7cf976c14dc142a2edc361c5bae233f8a1
KStefLarsson <mille83.sl@gmail.com>
2022-07-21 15:50:52 +0200

    Made a custom field for programs

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,7 +1,16 @@
 <?php 
-    // En funktion som skapar en ny post_type (custom_post_type) som syns i wordpress dashboard.
+    // Event Post Type
+    // En funktion som skapar en ny post_type (custom_post_type (event)) som syns i wordpress dashboard.
     function university_post_types() {
         register_post_type('event', array(
+            'show_in_rest' => true,
+            'supports' => array(
+                'title',
+                'editor',
+                'excerpt',
+            ),
+            'rewrite' => array('slug' => 'events'),
+            'has_archive' => true,
             'public' => true,       // Detta gör den synlig.
             'show_in_rest' => true,
             'labels' => array(
@@ -12,6 +21,28 @@
                 'singular_name' => 'Event'
             ),
             'menu_icon' => 'dashicons-calendar'  // Ändrar ikonen till en egen unik som jag själv väljer genom att googla på wordpress dashicons.
+        ));
+
+        // Program Post Type
+
+        register_post_type('program', array(
+            'show_in_rest' => true,
+            'supports' => array(
+                'title',
+                'editor'
+            ),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,       // Detta gör den synlig.
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'programs',  // Byter namn från Posts som är default namnet till Events.
+                'add_new_item' => 'Add New Program',  // Ändrar rubriken i wordpress dashboard från Add New Post till Add New Event.
+                'edit_item' => 'Edit Program',  // Ändrar rubriken i wordpress dashboard från Edit Post till Edit Event.
+                'all_items' => 'All Programs',  // Ändrar rubriken i wordpress dashboard menyn från Events till All Events.
+                'singular_name' => 'Program'
+            ),
+            'menu_icon' => 'dashicons-awards'  // Ändrar ikonen till en egen unik som jag själv väljer genom att googla på wordpress dashicons.
         ));
     }
 
