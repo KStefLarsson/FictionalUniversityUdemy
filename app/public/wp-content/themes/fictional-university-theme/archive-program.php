3c432689965aad30a44e9bbef2eab0c3d43d87a2
KStefLarsson <mille83.sl@gmail.com>
2022-07-22 23:34:09 +0200

    calling the pageBanner function on every page instead of typing the same code on every page.

--- app/public/wp-content/themes/fictional-university-theme/archive-program.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-program.php
@@ -1,20 +1,11 @@
 <?php 
 get_header();
-
+pageBanner(array(
+  'title' => 'All Programs',
+  'subtitle' => 'There is something for everyone. Have a look around.'
+));
 ?>
 
-<div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/barksalot.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            
-        <!-- Visar rubriken i headern olika beroende på vilken sida du besöker. -->
-            <h1 class="page-banner__title">All Programs</h1>
-            
-            <div class="page-banner__intro">
-            <p>There is something for everyone. Have a look around.</p>
-            </div>
-        </div>
-        </div>
 <div class="container container--narrow page-section">
   <ul class="link-list min-list">
     <?php 
--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -2,17 +2,9 @@
     get_header();
 
     while (have_posts()) {
-        the_post(); ?>
-
-        <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-                <div class="page-banner__content container container--narrow">
-                    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-                    <div class="page-banner__intro">
-                    <p>Dont forget to replace me later.</p>
-                    </div>
-            </div>
-        </div>
+        the_post(); 
+        pageBanner();
+        ?>
 
         <!-- en box på en event sida med knapp tillbaka till alla events och en ruta med namnet på eventet -->
         <div class="container container--narrow page-section">
