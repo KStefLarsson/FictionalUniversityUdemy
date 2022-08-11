dcf91daeb02c886693ee8431501336fafe272ccc
KStefLarsson <mille83.sl@gmail.com>
2022-08-12 00:35:22 +0200

    Added link to campus menu

--- app/public/wp-content/themes/fictional-university-theme/footer.php
+++ app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -23,7 +23,7 @@
                   <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                   <li><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
                   <li><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
-                  <li><a href="#">Campuses</a></li>
+                  <li><a href="<?php echo get_post_type_archive_link('campus') ?>">Campuses</a></li>
                 </ul>
               </nav>
             </div>
@@ -72,6 +72,21 @@
         </div>
       </div>
     </footer>
+
+    <div class="search-overlay">
+    <div class="search-overlay__top">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="container">
+      <div id="search-overlay__results">
+             
+      </div>
+    </div>
+  </div>
 
 <?php wp_footer(); ?>
 </body>
